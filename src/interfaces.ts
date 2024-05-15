6186c9761364c5212250029708aa9829383e2418
Tyo Drak <lptd99@gmail.com>
2024-05-15 12:50:45 -0400

    create interfaces.ts, add move existing interface definitions there

---
+++ src/interfaces.ts
@@ -0,0 +1,11 @@
+interface ICustomer {
+  id: number;
+  name: string;
+  city: string;
+  email: string;
+  phone: string;
+  address: string;
+  state: string;
+  postal_code: string;
+  country?: string;
+}
