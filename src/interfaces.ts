41f6befe7466622b7622e3b1776bd8c207072d59
Tyo Drak <lptd99@gmail.com>
2024-05-16 18:02:03 -0400

    Zustand adapt setup for use case

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,4 +1,4 @@
-interface ICustomer {
+export interface ICustomer {
   id: number;
   name: string;
   city: string;
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,11 +1,17 @@
+import { ICustomer } from "@/interfaces";
 import { create } from "zustand";
 
 type Store = {
   customerName: string;
   setCustomerName: () => void;
+  customerToUpdate: ICustomer | null;
+  setCustomerToUpdate: (customer: ICustomer | null) => void;
 };
 
 export const useStore = create<Store>()((set) => ({
   customerName: "",
   setCustomerName: () => set((state) => ({ customerName: state.customerName })),
+  customerToUpdate: null,
+  setCustomerToUpdate: (customer) =>
+    set(() => ({ customerToUpdate: customer })),
 }));
