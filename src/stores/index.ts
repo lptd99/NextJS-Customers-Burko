7109b32d2632115f4bc22f2fb0ddd52e0050e207
Tyo Drak <lptd99@gmail.com>
2024-05-16 19:02:41 -0400

    add persistence to Zustand store data

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,5 +1,6 @@
 import { ICustomer } from "@/interfaces";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 type Store = {
   customerName: string;
@@ -8,10 +9,19 @@ type Store = {
   setCustomerToUpdate: (customer: ICustomer | null) => void;
 };
 
-export const useStore = create<Store>()((set) => ({
-  customerName: "",
-  setCustomerName: () => set((state) => ({ customerName: state.customerName })),
-  customerToUpdate: null,
-  setCustomerToUpdate: (customer) =>
-    set(() => ({ customerToUpdate: customer })),
-}));
+export const useStore = create<Store>()(
+  persist(
+    (set) => ({
+      customerName: "",
+      setCustomerName: () =>
+        set((state) => ({ customerName: state.customerName })),
+      customerToUpdate: null,
+      setCustomerToUpdate: (customer) =>
+        set(() => ({ customerToUpdate: customer })),
+    }),
+    {
+      name: "customer-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
