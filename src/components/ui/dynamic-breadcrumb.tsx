bf590a92d7bc2618f35651ed9414ec6b3a1661ff
Tyo Drak <lptd99@gmail.com>
2024-05-16 19:26:03 -0400

    update implementation for easier use

--- src/components/ui/dynamic-breadcrumb.tsx
+++ src/components/ui/dynamic-breadcrumb.tsx
@@ -9,41 +9,43 @@ import { useRouter } from "next/navigation";
 
 interface IDynamicBreadcrumbProps {
   className: string;
-  texts: string[];
-  links: string[];
+  text: string;
+  link: string;
 }
 
 export default function DynamicBreadcrumb({
   className,
-  texts,
-  links,
+  text,
+  link,
 }: IDynamicBreadcrumbProps) {
+  const texts = text.split(", ");
+  const links = link.split(", ");
   const router = useRouter();
   return (
-      <Breadcrumb>
-        <BreadcrumbList>
-          {texts.map((text, index) => (
-            <>
-              {index < texts.length - 1 && (
-                <BreadcrumbItem key={index}>
-                  <BreadcrumbLink
-                    className={className}
-                    href={links[index]}>
-                    {text}
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-              )}
-              {index < texts.length - 1 && (
-                <BreadcrumbSeparator className={className} />
-              )}
-              {index === texts.length - 1 && (
-                <BreadcrumbItem key={index}>
-                  <BreadcrumbLink className={className}>{text}</BreadcrumbLink>
-                </BreadcrumbItem>
-              )}
-            </>
-          ))}
-        </BreadcrumbList>
-      </Breadcrumb>
+    <Breadcrumb>
+      <BreadcrumbList>
+        {texts.map((text, index) => (
+          <>
+            {index < texts.length - 1 && (
+              <BreadcrumbItem key={index}>
+                <BreadcrumbLink
+                  className={className}
+                  href={links[index]}>
+                  {text}
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+            )}
+            {index < texts.length - 1 && (
+              <BreadcrumbSeparator className={className} />
+            )}
+            {index === texts.length - 1 && (
+              <BreadcrumbItem key={index}>
+                <BreadcrumbLink className={className}>{text}</BreadcrumbLink>
+              </BreadcrumbItem>
+            )}
+          </>
+        ))}
+      </BreadcrumbList>
+    </Breadcrumb>
   );
 }
