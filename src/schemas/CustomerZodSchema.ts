4e8acc80834cee536f7bf7bd5355e0dd854ed098
Tyo Drak <lptd99@gmail.com>
2024-05-17 20:35:26 -0400

    update CustomerZodSchem validation criteria

--- src/schemas/CustomerZodSchema.ts
+++ src/schemas/CustomerZodSchema.ts
@@ -3,10 +3,10 @@ import { z } from "zod";
 export const CustomerZodSchema = z.object({
   name: z.string().min(6, { message: "Campo obrigatório" }),
   email: z.string().email({ message: "Campo obrigatório" }),
-  phone: z.string().min(10, { message: "Campo obrigatório" }),
+  phone: z.string().min(8, { message: "Campo obrigatório" }),
   address: z.string().min(6, { message: "Campo obrigatório" }),
-  city: z.string().min(3, { message: "Campo obrigatório" }),
+  city: z.string().min(2, { message: "Campo obrigatório" }),
   state: z.string().min(2, { message: "Campo obrigatório" }),
-  postal_code: z.string().min(6, { message: "Campo obrigatório" }),
-  country: z.string().min(3, { message: "Campo obrigatório" }).optional(),
+  country: z.string().min(2, { message: "Campo obrigatório" }),
+  postal_code: z.string().min(5, { message: "Campo obrigatório" }),
 });
