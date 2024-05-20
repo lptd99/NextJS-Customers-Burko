e1682f8ce5c1680a4320d00dbcc3dc7dcf016f11
Tyo Drak <lptd99@gmail.com>
2024-05-20 17:50:28 -0400

    add cpf data field

--- src/schemas/CustomerZodSchema.ts
+++ src/schemas/CustomerZodSchema.ts
@@ -4,6 +4,7 @@ export const CustomerZodSchema = z.object({
   name: z.string().min(6, { message: "Campo obrigatório" }),
   email: z.string().email({ message: "Campo obrigatório" }),
   phone: z.string().min(8, { message: "Campo obrigatório" }),
+  cpf: z.string().min(11, { message: "Campo obrigatório" }),
   address: z.string().min(6, { message: "Campo obrigatório" }),
   city: z.string().min(2, { message: "Campo obrigatório" }),
   state: z.string().min(2, { message: "Campo obrigatório" }),
