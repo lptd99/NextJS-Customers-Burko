ceef3f83b32a379c959802bfeb5808597d2a2ba1
Tyo Drak <lptd99@gmail.com>
2024-05-21 14:33:04 -0400

    reorder CustomerZodSchema properties for more accurate representation

--- src/schemas/CustomerZodSchema.ts
+++ src/schemas/CustomerZodSchema.ts
@@ -5,6 +5,8 @@ export const CustomerZodSchema = z.object({
   email: z.string().email({ message: "Campo obrigatório" }),
   phone: z.string().min(8, { message: "Campo obrigatório" }),
   cpf: z.string().min(11, { message: "Campo obrigatório" }),
+  country: z.string().min(2, { message: "Campo obrigatório" }),
+  postal_code: z.string().min(5, { message: "Campo obrigatório" }),
   address: z.string().min(6, { message: "Campo obrigatório" }),
   city: z.string().min(2, { message: "Campo obrigatório" }),
   state: z.string().min(2, { message: "Campo obrigatório" }),
