0709ca1719420e09911afc1dd1289eabd43b1a2e
Tyo Drak <lptd99@gmail.com>
2024-06-02 20:41:12 -0400

    add infiniteLoader for infiniteScroll component

---
+++ src/components/ui/infiniteLoader.tsx
@@ -0,0 +1,13 @@
+import { Spinner } from "./spinner";
+
+interface Props {}
+
+export default function InfiniteLoader(props: Props) {
+  const {} = props;
+
+  return (
+    <section>
+      <Spinner />
+    </section>
+  );
+}
